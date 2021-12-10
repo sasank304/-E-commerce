c4a8354f915df3b5f1e2b2ced7c463936624af74
sasank304 <86641553+sasank304@users.noreply.github.com>
2021-12-10 10:25:49 +0530

    Add files via upload

---
+++ Frontend/src/screens/CartScreen.js
@@ -0,0 +1,24 @@
+import './CartScreen.css';
+import CartItem from '../components/CartItem'
+const CartScreen = () => {
+    return <div className="cartscreen">
+            <div className="cartscreen_left">
+                <h2>Shopping cart</h2>
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cartscreen_right">
+                <div className="cartscreen_info">
+                    <p> Subtotal (0) items</p>
+                    <p>$499.99</p>
+                </div>
+                <div>
+                    <button>Proceed to Checkout</button>
+                </div>
+            </div>
+        </div >
+};
+
+export default CartScreen;
